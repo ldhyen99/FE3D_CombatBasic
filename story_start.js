a60ad2739547f91b47ecf895223ba464ef4d02ea
@yenldh <yenldh>
2021-10-30 02:11:16 +0700

    add animation for villain

--- story_start.js
+++ story_start.js
@@ -2,28 +2,66 @@ let hero = document.querySelector('#hero');
 let villain = document.querySelector('#villain');
 let lightning = document.querySelector('#lightning');
 let btnLetPlay = document.querySelector('#button_play');
-let options = { duration: 1000 }; // how long action should play for in this case; ms
 
 // focus goal good
 function focusGoalGood() {
   let lightningStart = { left: '290px' };
   let lightningEnd = { left: '900px' };
+  let options = { duration: 1000 }; // how long action should play for in this case; ms
+  let type = 'goodGoal';
 
-  resultLighting(lightningStart, lightningEnd);
+  resultLighting(lightningStart, lightningEnd, options, type);
 }
 
 // focus goal bad
 function focusGoalBad() {
   let lightningStart = { left: '290px' };
   let lightningEnd = { left: '900px', top: '70px' };
+  let options = { duration: 1000 }; // how long action should play for in this case; ms
+  let type = 'badGoal';
 
-  resultLighting(lightningStart, lightningEnd);
+  resultLighting(lightningStart, lightningEnd, options, type);
 }
 
-// random good or bad lightning
+// function receive start & end ligntning; return bad of good case
+function resultLighting(lightningStart, lightningEnd, options, type) {
+  btnLetPlay.disabled = true;
+  if (type === 'goodGoal') {
+    lightning.animate([lightningStart, lightningEnd], options).onfinish =
+      hitTheVillain;
+  }
+  lightning.animate([lightningStart, lightningEnd], options);
+  setTimeout(() => {
+    btnLetPlay.disabled = false;
+  }, options.duration);
+}
+
+// animation if we hit the villain
+function hitTheVillain() {
+  let villainStart = {
+    transform: 'rotate(0deg)',
+    opacity: 100,
+    transition: '0.4s',
+  };
+  let villainEnd = { transform: 'rotate(1000deg)', opacity: 0 };
+
+  let options = { duration: 1500 };
+
+  actionOfVillain(villainStart, villainEnd, options);
+}
+
+// action of Villain character
+function actionOfVillain(villainStart, villainEnd, options) {
+  btnLetPlay.disabled = true;
+  villain.animate([villainStart, villainEnd], options);
+  setTimeout(() => {
+    btnLetPlay.disabled = false;
+  }, options.duration);
+}
+
+// random good or bad lightning by onclick
 function randomTypelightning() {
   const numbRandom = Math.floor(Math.random() * 2);
-  console.log({ numbRandom });
 
   if (numbRandom === 0) {
     focusGoalGood();
@@ -31,13 +69,4 @@ function randomTypelightning() {
   }
 
   focusGoalBad();
-}
-
-// function receive start & end ligntning; return bad of good case
-function resultLighting(lightningStart, lightningEnd) {
-  btnLetPlay.disabled = true;
-  lightning.animate([lightningStart, lightningEnd], options);
-  setTimeout(() => {
-    btnLetPlay.disabled = false;
-  }, options.duration);
 }
